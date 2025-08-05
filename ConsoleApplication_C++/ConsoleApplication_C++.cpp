ba79521b3023c8258bead6add770ba1829e48fad
Aleksandr Maliatov <maltxwest@gmail.com>
2025-08-05 16:19:26 +0200

    #94 Statische Methoden

--- ConsoleApplication_C++/ConsoleApplication_C++.cpp
+++ ConsoleApplication_C++/ConsoleApplication_C++.cpp
@@ -10,25 +10,42 @@ using namespace std;
 class Apple {
     public:
 
-     static int count; // static variable, die gemeinsam für alle Objekte ist.
-     void foo(int val) {
-         count = val;
-        cout << "hi" << endl;
-        count++;
+        static int GetCount() {
+            cout << count << endl;              // Zugriff auf die statische Variable
+			return count;                           
+           
+        }
+        static void SetCount(int& val) {
+
+            count = val;                           // Zugriff auf die statische Variable
+            
+        }
+
+     
+     void foo() {         
+        cout << "hi" << endl;       
         cout << count << endl;
     }
-
+private:
+    static int count;
 
 
 };
-int Apple::count;       // !!! man muss unbedingt eine static Variable definieren!!! 
+int Apple::count = 0;       // !!! man muss unbedingt eine static Variable definieren!!! f
 
 int main()
 {
    // 94 Статические методы класса зачем нужны.Модификатор static.Как влияет.ООП.Для начинающих.Урок #94
    Apple app;    // zugriff auf einer static Variable, direkt aus einer Klasse! 
-   app.foo(5);
-   Apple::count++;
+
+   int val = 16;
+
+
+   Apple::SetCount(val);
+   Apple::GetCount();
+
+
+  // app.foo(); 
    
    return 0;
 }
